9a59361d514ca39e72c38d57a8f853c7e69cad8c
saadeghi <saadeghi@users.noreply.github.com>
2025-04-12 00:06:30 +0000

    deploy: d51b58ae9409ddec44bd536ed47c866b70728466

--- _app/immutable/nodes/26.DEKLz2dP.js
+++
@@ -1,6 +0,0 @@
-import{t as s,a as t,b as n}from"../chunks/Dq2fu_cU.js";import"../chunks/BEeyPY_Q.js";import{$ as m,n as e}from"../chunks/CvpbVIua.js";import{h as l}from"../chunks/CRuugIuz.js";var p=n(`<meta name="robots" content="noindex"> <link rel="shortcut icon" href="data:image/x-icon;," type="image/x-icon"> <script src="https://lmsqueezy.com/affiliate.js" defer><\/script> <style>html,
-    body {
-      color-scheme: light dark;
-      margin: 0;
-      padding: 0;
-    }</style>`,1),c=s('<div style="display: grid; place-items: center; height: 100vh; font-family: sans-serif;"><svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle cx="12" cy="12" r="10" fill="none" stroke="currentColor" stroke-width="2" opacity="0.2"></circle><path d="M12 2a10 10 0 0 1 10 10" fill="none" stroke="currentColor" stroke-width="2"><animateTransform attributeName="transform" type="rotate" from="0 12 12" to="360 12 12" dur="1s" repeatCount="indefinite"></animateTransform></path></svg></div> <div style="position: absolute; bottom: 2rem; left: 2rem; font-size: 11px; opacity: 0.3; font-family: sans-serif;">If this is not loading, <br>please disable Ad Blockers.</div>',1);function y(o){var r=c();l(i=>{var a=p();m.title="Loading...",e(6),t(i,a)}),e(2),t(o,r)}export{y as component};
